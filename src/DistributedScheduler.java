fae3296d0f166e1ecc5b70083986c6753655162f
Muhammed <mmusabgldmr@gmail.com>
2026-01-14 21:30:09 +0300

    .
    .

--- src/DistributedScheduler.java
+++ src/DistributedScheduler.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.UUID;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,12 +11,14 @@ import javax.management.loading.PrivateClassLoader;
 import javax.swing.plaf.basic.BasicScrollPaneUI.ViewportChangeHandler;
 import javax.swing.plaf.basic.BasicTreeUI.NodeDimensionsHandler;
 import javax.swing.text.MaskFormatter;
+import javax.swing.text.TabSet;
 
 import java.beans.beancontext.BeanContext;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.classfile.instruction.StoreInstruction;
 import java.lang.invoke.StringConcatFactory;
 import java.net.FileNameMap;
 import java.net.ServerSocket;
@@ -95,4 +98,113 @@ public class DistributedScheduler {
             });
         }
     }
+
+    private static void startMasterNode() throws Exception {
+        System.out.println("\n👑 MASTER NODE BAŞLATILIYOR");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Master adi: ");
+        String name = scanner.nextLine();
+        System.out.println("Port numarasi(9090): ");
+        int port = scanner.nextInt();
+        scanner.nextLine();
+
+        MasterNode master = new MasterNode(name, port);
+        master.start();
+
+        // Konsol arayüz
+
+        while (true) {
+            System.out.println("\n=== MASTER KONSOLU ===");
+            System.out.println("1. Yeni görev ekle");
+            System.out.println("2. Tüm görevleri listele");
+            System.out.println("3. Worker'lari listele");
+            System.out.println("4. Görev gönder");
+            System.out.println("5. Çikiş");
+            System.out.print("Seçimi: ");
+            scanner.nextLine();
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch(choice) {
+                case 1:
+                    System.out.print("Görev adi: ");
+                    String taskName = scanner.nextLine();
+                    System.out.print("Komut: ");
+                    String command = scanner.nextLine();
+                    int priority = scanner.nextInt();
+                    scanner.nextLine(); 
+
+                    Task task = new Task(taskName, command, priority);
+                    master.addTask(task);
+                    System.out.println("✅ Görev eklendi: " + task.id);
+
+                    break;
+
+                case 2:
+                    master.lisTasks();
+                    break;
+                
+                case 3:
+                    master.listWorkers();
+                    break;
+
+                case 4:
+                    if (master.hasTasks() && master.hasWorkers()) {
+                        System.out.println("Gönderilecek görev ID: ");
+                        String taskId = scanner.nextLine();
+                        master.sendTaskToWorker(taskId);
+                    } else {
+                        System.out.println("⚠️ Görev veya Worker yok!");
+                    }
+
+                    break;
+
+                case 5:
+                    master.stop();
+                    System.out.println("Master durduruldu");
+                    scanner.close();
+                    return;
+            }
+        }
+    }
+
+    private static void startWorkerNode() throws Exception {
+        System.out.println("\n🔧 WORKER NODE BAŞLATILIYOR");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Worker adi: ");
+        String name = scanner.nextLine();
+        System.out.print("Master adresi (localhost): ");
+        String masterHost = scanner.nextLine();
+        if (masterHost.isEmpty()) masterHost = "localhost";
+
+        System.out.print("Master port (9090): ");
+        int masterPort = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Worker port (9091): ");
+        int workerPort = scanner.nextInt();
+        scanner.nextLine();
+
+        WorkerNode worker = new WorkerNode(name, masterHost, masterPort, workerPort);
+        worker.start();
+
+        System.out.println("\n✅ Worker başlatildi! Master'dan görev bekleniyor...");
+        System.out.println("Çikmak için 'exit' yazin. ");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                worker.stop();
+                System.out.println("Worker durduruldu. ");
+                scanner.close();
+                return;
+            }
+        }
+    }
 }
+
+
+
---
+++ src/Task.java
@@ -0,0 +1,40 @@
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+class Task {
+    String id;
+    String name;
+    String command;
+    int priority;
+    String status;
+    String assignedWorker;
+    LocalDateTime createdAt;
+
+    public Task(String name, String command, int priority) {
+        this.id = "TASK-" + UUID.randomUUID().toString().substring(0,8);
+        this.name = name;
+        this.command = command;
+        this.priority = priority;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[%s] %s (Öncelik: %d) - Durum %s", id,
+            name, priority, status) 
+   }
+
+   public String toNetworkString() {
+    return String.format("%s|%s|%s|%d", id, name, command, priority)
+
+   }
+
+   public static Task fromNerworkString(String newworkString) {
+
+    String[] parts = newworkString.split("\\|");
+    Task task = new Task(parts[1], parts[2], Integer.parseInt(parts[3]));
+    task.id = parts[0];
+    return task;
+   }
+}
